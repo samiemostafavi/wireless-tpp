d999681192f3b2fe6ece4a9d9b8aacef0ed0f11b
Samie Mostafavi <ssmos@kth.se>
2024-10-30 12:57:28 -0400

    fix wrapper

--- easy_tpp/torch_wrapper.py
+++ easy_tpp/torch_wrapper.py
@@ -1,43 +1,52 @@
-""" Initialize a Pytorch model wrapper that feed into Model Runner   """
+""" Initialize a Tf model wrapper that feed into Model Runner   """
 
-import torch
-from torch.utils.tensorboard import SummaryWriter
+import tensorflow as tf
 
-from easy_tpp.utils import RunnerPhase, set_optimizer, set_device
+from easy_tpp.utils import RunnerPhase
+from easy_tpp.utils.tf_utils import set_device, set_optimizer
+
+if tf.__version__ >= '2.0':
+    tf = tf.compat.v1
+    tf.disable_v2_behavior()
 
 
-class TorchModelWrapper:
+class TfModelWrapper:
     def __init__(self, model, base_config, model_config, trainer_config):
-        """A wrapper class for Torch backends.
+        """A wrapper class for Tensorflow backends.
 
         Args:
             model (BaseModel): a TPP model.
             base_config (EasyTPP.Config): basic configs.
-            model_config (EasyTPP.ModelConfig): model spec configs.
-            trainer_config (EasyTPP.TrainerConfig): trainer spec configs.
+            model_config (EasyTPP.Config): model spec configs.
+            trainer_config (EasyTPP.Config): trainer spec configs.
         """
         self.model = model
         self.base_config = base_config
         self.model_config = model_config
         self.trainer_config = trainer_config
+        set_device(self.trainer_config.gpu)
 
-        self.model_id = self.base_config.model_id
-        self.device = set_device(self.trainer_config.gpu)
-
-        self.model.to(self.device)
-
+        # init session and build model
+        tf.reset_default_graph()
+        self.sess = tf.Session()
+        self.model.build_graph()
         if self.model_config.is_training:
             # set up optimizer
             optimizer = self.trainer_config.optimizer
             self.learning_rate = self.trainer_config.learning_rate
-            self.opt = set_optimizer(optimizer, self.model.parameters(), self.learning_rate)
+            self.opt = set_optimizer(optimizer, self.learning_rate)
+            self.train_op = self.opt.minimize(self.model.loss)
 
         # set up tensorboard
         self.use_tfb = self.trainer_config.use_tfb
         self.train_summary_writer, self.valid_summary_writer = None, None
         if self.use_tfb:
-            self.train_summary_writer = SummaryWriter(log_dir=self.base_config.spec['tfb_train_dir'])
-            self.valid_summary_writer = SummaryWriter(log_dir=self.base_config.spec['tfb_valid_dir'])
+            self.train_summary_writer = tf.summary.FileWriter(self.base_config.spec['tfb_train_dir'])
+            self.valid_summary_writer = tf.summary.FileWriter(self.base_config.spec['tfb_valid_dir'])
+
+        # init variable and saver
+        self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
 
     def restore(self, ckpt_dir):
         """Load the checkpoint to restore the model.
@@ -45,8 +54,7 @@ class TorchModelWrapper:
         Args:
             ckpt_dir (str): path for the checkpoint.
         """
-
-        self.model.load_state_dict(torch.load(ckpt_dir), strict=False)
+        self.saver.restore(self.sess, ckpt_dir)
 
     def save(self, ckpt_dir):
         """Save the checkpoint for the model.
@@ -54,10 +62,10 @@ class TorchModelWrapper:
         Args:
             ckpt_dir (str): path for the checkpoint.
         """
-        torch.save(self.model.state_dict(), ckpt_dir)
+        self.saver.save(self.sess, ckpt_dir)
 
     def write_summary(self, epoch, kv_pairs, phase):
-        """Write the kv_paris into the tensorboard
+        """Write the kv_paris into the tensorboard.
 
         Args:
             epoch (int): epoch index in the training.
@@ -73,10 +81,12 @@ class TorchModelWrapper:
             elif phase == RunnerPhase.PREDICT:
                 pass
 
+            metric_summary = tf.Summary()
             if summary_writer is not None:
                 for k, v in kv_pairs.items():
                     if k != 'num_events':
-                        summary_writer.add_scalar(k, v, epoch)
+                        metric_summary.value.add(tag=k, simple_value=v)
+                        summary_writer.add_summary(metric_summary, epoch)
 
                 summary_writer.flush()
         return
@@ -102,74 +112,53 @@ class TorchModelWrapper:
             tuple: for training and validation we return loss, prediction and labels;
             for prediction we return prediction.
         """
+        model = self.model
+        sess = self.sess
 
-        batch = batch.to(self.device).values()
+        time_seqs, time_delta_seqs, type_seqs, batch_non_pad_mask, attention_mask, type_mask = batch.data.values()
+
+        # set mode to train
+        is_training = (phase == RunnerPhase.TRAIN)
+
+        fd = {
+            model.time_seqs: time_seqs,
+            model.time_delta_seqs: time_delta_seqs,
+            model.type_seqs: type_seqs,
+            model.batch_non_pad_mask: batch_non_pad_mask,
+            model.attention_mask: attention_mask,
+            model.type_mask: type_mask,
+            model.is_training: is_training
+
+        }
+
+        # Assume we dont do prediction on train set
+        pred_dtime, pred_type = None, None
+        label_dtime, label_type = time_delta_seqs[:, 1:], type_seqs[:, 1:]
+
+        mask = batch_non_pad_mask[:, 1:]
+
         if phase in (RunnerPhase.TRAIN, RunnerPhase.VALIDATE):
             # set mode to train
-            is_training = (phase == RunnerPhase.TRAIN)
-            self.model.train(is_training)
-
-            # FullyRNN needs grad event in validation stage
-            grad_flag = is_training if not self.model_id == 'FullyNN' else True
-            # run model
-            with torch.set_grad_enabled(grad_flag):
-                loss, num_event = self.model.loglike_loss(batch)
-
-            # Assume we dont do prediction on train set
-            pred_dtime, pred_type, label_dtime, label_type, mask = None, None, None, None, None
-
-            # update grad
             if is_training:
-                self.opt.zero_grad()
-                (loss / num_event).backward()
-                self.opt.step()
-            else:  # by default we do not do evaluation on train set which may take a long time
+                _, loss, num_event = sess.run([self.train_op,
+                                               model.loss,
+                                               model.num_event],
+                                              feed_dict=fd)
+            else:
+                loss, num_event = sess.run([model.loss,
+                                            model.num_event],
+                                           feed_dict=fd)
+
                 if self.model.event_sampler:
-                    self.model.eval()
-                    with torch.no_grad():
-                        if batch[1] is not None and batch[2] is not None:
-                            label_dtime, label_type = batch[1][:, 1:].cpu().numpy(), batch[2][:, 1:].cpu().numpy()
-                        if batch[3] is not None:
-                            mask = batch[3][:, 1:].cpu().numpy()
-                        pred_dtime, pred_type = self.model.predict_one_step_at_every_event(batch=batch)
-                        pred_dtime = pred_dtime.detach().cpu().numpy()
-                        pred_type = pred_type.detach().cpu().numpy()
-            return loss.item(), num_event, (pred_dtime, pred_type), (label_dtime, label_type), (mask,)
+                    pred_dtime, pred_type = sess.run([model.dtime_predict_one_step,
+                                                      model.type_predict_one_step],
+                                                     feed_dict=fd)
+            return loss, num_event, (pred_dtime, pred_type), (label_dtime, label_type), (mask,)
         else:
-            pred_dtime, pred_type, label_dtime, label_type = self.model.predict_multi_step_since_last_event(batch=batch)
-            pred_dtime = pred_dtime.detach().cpu().numpy()
-            pred_type = pred_type.detach().cpu().numpy()
-            label_dtime = label_dtime.detach().cpu().numpy()
-            label_type = label_type.detach().cpu().numpy()
+            pred_dtime, pred_type = sess.run([model.dtime_generation,
+                                              model.type_generation],
+                                             feed_dict=fd)
+            num_steps = pred_dtime.shape[-1]
+            label_dtime = time_delta_seqs[:, -num_steps:]
+            label_type = type_seqs[:, -num_steps:]
             return (pred_dtime, pred_type), (label_dtime, label_type)
-
-
-    def run_batch_probability(self, batch, phase):
-        """Run one batch get probabilities only for the last event in the sequence
-
-        Args:
-            batch (EasyTPP.BatchEncoding): preprocessed batch data that go into the model.
-            phase (RunnerPhase): a const that defines the stage of model runner.
-
-        Returns:
-            tuple: for training and validation we return loss, prediction and labels;
-            for prediction we return prediction.
-        """
-
-        batch = batch.to(self.device).values()
-        if phase is not RunnerPhase.PREDICT:
-            return None
-        
-        # [batch_size, seq_len, num_samples_boundary, event_num]
-        dtimes_probs_pred, types_probs_pred, dtime_samples, dtimes_mean_pred, types_mean_pred, label_time, label_dtime, label_type = self.model.predict_probabilities_one_step_since_last_event(batch=batch)
-        ll_loss, _ = self.model.loglike_loss(batch=batch)
-        loglikelihood = (-ll_loss).detach().cpu().numpy()
-        dtimes_probs_pred = dtimes_probs_pred.detach().cpu().numpy()
-        dtime_samples = dtime_samples.detach().cpu().numpy() 
-        types_probs_pred = types_probs_pred.detach().cpu().numpy()
-        dtimes_mean_pred = dtimes_mean_pred.detach().cpu().numpy()
-        types_mean_pred = types_mean_pred.detach().cpu().numpy()
-        label_dtime = label_dtime.detach().cpu().numpy()
-        label_time = label_time.detach().cpu().numpy()
-        label_type = label_type.detach().cpu().numpy()
-        return dtimes_probs_pred, types_probs_pred, dtime_samples, loglikelihood, dtimes_mean_pred, types_mean_pred, (label_time, label_dtime, label_type)
