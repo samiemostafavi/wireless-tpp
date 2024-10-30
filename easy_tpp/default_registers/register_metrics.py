d85d65b3afd61ba46467cdad22a406fcd5a159ea
Samie Mostafavi <ssmos@kth.se>
2024-10-30 12:40:06 -0400

    add 2D intensity free and evaluations

--- easy_tpp/default_registers/register_metrics.py
+++ easy_tpp/default_registers/register_metrics.py
@@ -4,6 +4,134 @@ from easy_tpp.utils.const import PredOutputIndex
 from easy_tpp.utils.metrics import MetricsHelper
 
 
+@MetricsHelper.register(name='rmse_2d', direction=MetricsHelper.MINIMIZE, overwrite=False)
+def rmse_2d_metric_function(predictions, labels, **kwargs):
+    """Compute rmse metrics of the time predictions.
+
+    Args:
+        predictions (np.array): model predictions.
+        labels (np.array): ground truth.
+
+    Returns:
+        float: average rmse of the time predictions.
+    """
+    # input prediction shape is [1, 2, batch_size, seq_len, 2]
+    # we pick the last element of the sequence and remove the first 2 dims
+    # result is [batch_size, seq_len, 2]
+    predictions = np.array(predictions)[0,0,:,-1,:]
+
+    # input label shape is [1, 2, batch_size, seq_len]
+    # we pick the last element of the sequence and remove the first dim, then we transpose the matrix
+    labels = np.array(labels)[0,:,:,-1].T
+
+    # Calculate RMSE for X and Y separately
+    rmse_dtime = np.sqrt(np.mean((labels[:, 0] - predictions[:, 0]) ** 2))
+    rmse_event = np.sqrt(np.mean((labels[:, 1] - predictions[:, 1]) ** 2))
+
+    # Overall RMSE considering both X and Y together
+    overall_rmse = np.sqrt(np.mean(np.sum((labels - predictions) ** 2, axis=1)))
+
+    return overall_rmse
+
+@MetricsHelper.register(name='rmse_2d_dtime', direction=MetricsHelper.MINIMIZE, overwrite=False)
+def rmse_2d_dtime_metric_function(predictions, labels, **kwargs):
+    """Compute rmse metrics of the time predictions.
+
+    Args:
+        predictions (np.array): model predictions.
+        labels (np.array): ground truth.
+
+    Returns:
+        float: average rmse of the time predictions.
+    """
+    # input prediction shape is [1, 2, batch_size, seq_len, 2]
+    # we pick the last element of the sequence and remove the first 2 dims
+    # result is [batch_size, seq_len, 2]
+    predictions = np.array(predictions)[0,0,:,-1,:]
+
+    # input label shape is [1, 2, batch_size, seq_len]
+    # we pick the last element of the sequence and remove the first dim, then we transpose the matrix
+    labels = np.array(labels)[0,:,:,-1].T
+
+    # Calculate RMSE for X and Y separately
+    rmse_dtime = np.sqrt(np.mean((labels[:, 0] - predictions[:, 0]) ** 2))
+
+    return rmse_dtime
+
+@MetricsHelper.register(name='rmse_2d_event', direction=MetricsHelper.MINIMIZE, overwrite=False)
+def rmse_2d_event_metric_function(predictions, labels, **kwargs):
+    """Compute rmse metrics of the time predictions.
+
+    Args:
+        predictions (np.array): model predictions.
+        labels (np.array): ground truth.
+
+    Returns:
+        float: average rmse of the time predictions.
+    """
+    # input prediction shape is [1, 2, batch_size, seq_len, 2]
+    # we pick the last element of the sequence and remove the first 2 dims
+    # result is [batch_size, seq_len, 2]
+    predictions = np.array(predictions)[0,0,:,-1,:]
+
+    # input label shape is [1, 2, batch_size, seq_len]
+    # we pick the last element of the sequence and remove the first dim, then we transpose the matrix
+    labels = np.array(labels)[0,:,:,-1].T
+
+    # Calculate RMSE for X and Y separately
+    rmse_event = np.sqrt(np.mean((labels[:, 1] - predictions[:, 1]) ** 2))
+
+    return rmse_event
+
+@MetricsHelper.register(name='rmse_dtime', direction=MetricsHelper.MINIMIZE, overwrite=False)
+def rmse_dtime_metric_function(predictions, labels, **kwargs):
+    """Compute rmse metrics of 1D time predictions.
+
+    Args:
+        predictions (np.array): model predictions.
+        labels (np.array): ground truth.
+
+    Returns:
+        float: average rmse of the time predictions.
+    """
+    # input prediction shape is [1, 2, batch_size, seq_len]
+    # we pick the last element of the sequence and remove the first 2 dims
+    # result is [batch_size]
+    predictions = np.array(predictions)[0,0,:,-1]
+
+    # input label shape is [1, 2, batch_size, seq_len]
+    # we pick the last element of the sequence and remove the first dim, then we transpose the matrix\
+    # result is [batch_size]
+    labels = np.array(labels)[0,0,:,-1]
+
+    # Overall RMSE considering both X and Y together
+    return np.sqrt(np.mean((labels - predictions) ** 2))
+
+
+@MetricsHelper.register(name='rmse_event', direction=MetricsHelper.MINIMIZE, overwrite=False)
+def rmse_event_metric_function(predictions, labels, **kwargs):
+    """Compute rmse metrics of 1D event predictions.
+
+    Args:
+        predictions (np.array): model predictions.
+        labels (np.array): ground truth.
+
+    Returns:
+        float: average rmse of the time predictions.
+    """
+    # input prediction shape is [1, 2, batch_size, seq_len]
+    # we pick the last element of the sequence and remove the first 2 dims
+    # result is [batch_size]
+    predictions = np.array(predictions)[0,1,:,-1]
+
+    # input label shape is [1, 2, batch_size, seq_len]
+    # we pick the last element of the sequence and remove the first dim, then we transpose the matrix\
+    # result is [batch_size]
+    labels = np.array(labels)[0,1,:,-1]
+
+    # Overall RMSE considering both X and Y together
+    return np.sqrt(np.mean((labels - predictions) ** 2))
+
 
 @MetricsHelper.register(name='rmse', direction=MetricsHelper.MINIMIZE, overwrite=False)
 def rmse_metric_function(predictions, labels, **kwargs):
@@ -45,11 +173,11 @@ def acc_metric_function(predictions, labels, **kwargs):
 
     # apply the mask and remove the first element since it is repeated
     if len(seq_mask) > 0:
-        pred = np.array(predictions[PredOutputIndex.TimePredIndex][seq_mask])
-        label = np.array(labels[PredOutputIndex.TimePredIndex][seq_mask])
+        pred = np.array(predictions[PredOutputIndex.TypePredIndex][seq_mask])
+        label = np.array(labels[PredOutputIndex.TypePredIndex][seq_mask])
     else:
-        pred = np.array(predictions[PredOutputIndex.TimePredIndex])[:, 1:]
-        label = np.array(labels[PredOutputIndex.TimePredIndex])[:, 1:]
+        pred = np.array(predictions[PredOutputIndex.TypePredIndex])[:, 1:]
+        label = np.array(labels[PredOutputIndex.TypePredIndex])[:, 1:]
 
     pred = np.reshape(pred, [-1])
     label = np.reshape(label, [-1])
--- easy_tpp/model/__init__.py
+++ easy_tpp/model/__init__.py
@@ -3,6 +3,7 @@ from easy_tpp.model.torch_model.torch_attnhp import AttNHP as TorchAttNHP
 from easy_tpp.model.torch_model.torch_basemodel import TorchBaseModel
 from easy_tpp.model.torch_model.torch_fullynn import FullyNN as TorchFullyNN
 from easy_tpp.model.torch_model.torch_intensity_free import IntensityFree as TorchIntensityFree
+from easy_tpp.model.torch_model.torch_intensity_free_2d import IntensityFree2D as TorchIntensityFree2D
 from easy_tpp.model.torch_model.torch_nhp import NHP as TorchNHP
 from easy_tpp.model.torch_model.torch_ode_tpp import ODETPP as TorchODETPP
 from easy_tpp.model.torch_model.torch_rmtpp import RMTPP as TorchRMTPP
@@ -31,6 +32,7 @@ __all__ = ['TorchBaseModel',
            'TorchSAHP',
            'TorchFullyNN',
            'TorchIntensityFree',
+           'TorchIntensityFree2D',
            'TorchODETPP',
            'TfBaseModel',
            'TfNHP',
