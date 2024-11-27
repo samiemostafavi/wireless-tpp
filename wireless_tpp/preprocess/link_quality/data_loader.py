70b66ce76dc915bc0e1d95851262f53e318bf4fa
Samie Mostafavi <ssmos@kth.se>
2024-11-27 12:24:52 -0500

    fix stuff

--- wireless_tpp/preprocess/link_quality/data_loader.py
+++ wireless_tpp/preprocess/link_quality/data_loader.py
@@ -1,6 +1,9 @@
 from wireless_tpp.preprocess import BaseTPPDataLoader
 from wireless_tpp.utils import load_pickle, py_assert
+from wireless_tpp.preprocess import get_data_loader
+from wireless_tpp.preprocess import EventTokenizer
 
+from .dataset import TPPDatasetLinkQuality
 
 class TPPDataLoaderLinkQuality(BaseTPPDataLoader):
     def __init__(self, data_config, backend, **kwargs):
@@ -76,3 +79,35 @@ class TPPDataLoaderLinkQuality(BaseTPPDataLoader):
             input_dict = dict({'time_seqs': time_seqs, 'time_delta_seqs': time_delta_seqs, 'type_seqs': type_seqs})
         return input_dict
 
+
+    def get_loader(self, split='train', **kwargs):
+        """Get the corresponding data loader.
+
+        Args:
+            split (str, optional): denote the train, valid and test set. Defaults to 'train'.
+            num_event_types (int, optional): num of event types in the data. Defaults to None.
+
+        Raises:
+            NotImplementedError: the input of 'num_event_types' is inconsistent with the data.
+
+        Returns:
+            EasyTPP.DataLoader: the data loader for tpp data.
+        """
+        data_dir = self.data_config.get_data_dir(split)
+        data_source_type = data_dir.split('.')[-1]
+
+        if data_source_type == 'pkl':
+            data = self.build_input_from_pkl(data_dir, split)
+        else:
+            data = self.build_input_from_json(data_dir, split)
+
+        dataset = TPPDatasetLinkQuality(data)
+        tokenizer = EventTokenizer(self.data_config.data_specs)
+        loader = get_data_loader(dataset,
+                                 self.backend,
+                                 tokenizer,
+                                 batch_size=self.kwargs['batch_size'],
+                                 shuffle=self.kwargs['shuffle'],
+                                 **kwargs)
+
+        return loader
