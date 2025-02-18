5851c638af8a611c225f7aaed53980aa1795a7ec
Samie Mostafavi <ssmos@kth.se>
2025-02-18 11:32:00 -0500

    add singlestep prediction lstm

--- wireless_tpp/model/__init__.py
+++ wireless_tpp/model/__init__.py
@@ -3,7 +3,7 @@ from wireless_tpp.model.basemodel import TorchBaseModel
 from wireless_tpp.model.mdn import *
 
 from wireless_tpp.model.e2e_delay.transformer_e2e import TransformerE2E
-from wireless_tpp.model.e2e_delay.rnn_e2e import RecurrentE2E
+from wireless_tpp.model.e2e_delay.rnn_e2e import RecurrentE2E, RecurrentE2ESingle
 from wireless_tpp.model.e2e_delay.mlp_e2e import MLPE2E
 
 
@@ -11,5 +11,6 @@ __all__ = [
     'TorchBaseModel',
     'TransformerE2E',
     'RecurrentE2E',
+    'RecurrentE2ESingle',
     'MLPE2E'
 ]
--- wireless_tpp/model/e2e_delay/rnn_e2e.py
+++ wireless_tpp/model/e2e_delay/rnn_e2e.py
@@ -336,3 +336,268 @@ class RecurrentE2E(TorchBaseModel):
         assert label.shape == pred_var.shape
 
         return (pred_mean, pred_var, pred_q5a, pred_q5b, pred_q7a, pred_q7b, pred_q9a, pred_q9b, pred_q99a, pred_q99b), label, (interarrival_time_src_seqs, len_src_seqs), pred_mask
+    
+
+
+
+
+class RecurrentE2ESingle(TorchBaseModel):
+    """Torch implementation of MDN Learning of Temporal Point Processes
+    """
+
+    def __init__(self, model_config):
+        """Initialize the model
+
+        Args:
+            model_config (EasyTPP.ModelConfig): config of model specs.
+
+        """
+        super(RecurrentE2ESingle, self).__init__(model_config)
+
+
+        assert model_config.model_specs['rnn_type'] == 'lstm' or model_config.model_specs['rnn_type'] == 'gru' or model_config.model_specs['rnn_type'] == 'rnn'
+        self.rnn_type = model_config.model_specs['rnn_type']
+        self.num_layers = model_config.model_specs['num_layers']
+        self.bidirectional = bool(model_config.model_specs['bidirectional'])
+        logger.info(f"RNN type: {self.rnn_type}, num_layers: {self.num_layers}, bidirectional: {self.bidirectional}")
+
+        # size of transformer tokens stays fixed
+        self.d_model = model_config.hidden_size
+        self.use_norm = model_config.use_ln
+        self.dropout = model_config.dropout_rate
+
+        self.tgt_seq_len = model_config.model_specs['tgt_seq_len']
+        self.src_seq_len = model_config.model_specs['src_seq_len']
+        
+        self.PAD_TOKEN = -1.0
+
+        # slots embedding
+        self.num_slots_types = model_config.model_specs['types_and_paddings']['slot'][0]  
+        self.slots_pad_id = model_config.model_specs['types_and_paddings']['slot'][1]
+        # mcs embedding
+        self.num_mcs_types = model_config.model_specs['types_and_paddings']['mcs'][0]  
+        self.mcs_pad_id = model_config.model_specs['types_and_paddings']['mcs'][1]
+        # retransmissions embedding
+        self.num_mretx_types = model_config.model_specs['types_and_paddings']['mretx'][0]  
+        self.mretx_pad_id = model_config.model_specs['types_and_paddings']['mretx'][1]
+        # rlc failed embedding
+        self.num_rfailed_types = model_config.model_specs['types_and_paddings']['rfailed'][0]  
+        self.rfailed_pad_id = model_config.model_specs['types_and_paddings']['rfailed'][1]
+        # rum rbs embedding
+        self.num_rbs_types = model_config.model_specs['types_and_paddings']['num_rbs'][0] 
+        self.rbs_pad_id = model_config.model_specs['types_and_paddings']['num_rbs'][1]
+
+        self.delay_embedding = DelayEmbedding(
+            d_model=self.d_model,
+            device = self.device,
+            model_config = model_config
+        )
+
+        # MixtureDistribution
+        self.mdn = MixtureDistribution(model_config, self.device)
+
+        self.include_prev_dtime_in_tgt = model_config.model_specs['target']['include_prev_dtime']
+        self.include_slot_in_tgt = model_config.model_specs['target']['include_slot']
+        self.include_mcs_in_tgt = model_config.model_specs['target']['include_mcs']
+        self.include_mretx_in_tgt = model_config.model_specs['target']['include_mretx']
+        self.include_rfailed_in_tgt = model_config.model_specs['target']['include_rfailed']
+        self.PAD_TOKEN = -1.0
+
+        # RNN layer definition
+        if self.rnn_type == 'lstm':
+            self.layer_rnn = nn.LSTM(
+                input_size=self.d_model,
+                hidden_size=self.d_model,
+                dropout=self.dropout,
+                num_layers=self.num_layers,
+                bidirectional=self.bidirectional,
+                device=self.device,
+                batch_first=True
+            )
+        elif self.rnn_type == 'gru':
+            self.layer_rnn = nn.GRU(
+                input_size=self.d_model,
+                hidden_size=self.d_model,
+                dropout=self.dropout,
+                num_layers=self.num_layers,
+                bidirectional=self.bidirectional,
+                device=self.device,
+                batch_first=True
+            )
+        elif self.rnn_type == 'rnn':
+            self.layer_rnn = nn.RNN(
+                input_size=self.d_model,
+                hidden_size=self.d_model,
+                dropout=self.dropout,
+                num_layers=self.num_layers,
+                bidirectional=self.bidirectional,
+                device=self.device,
+                batch_first=True
+            )
+
+        if self.bidirectional:
+            self.mdnhead_input_size = 2 * self.d_model
+        else:
+            self.mdnhead_input_size = self.d_model
+
+
+        self.mdn_head = nn.Linear(self.mdnhead_input_size, self.mdn.num_params, device=self.device)
+
+
+    def embed(self, seq_obj : SequenceSeperate):
+        """Call the model
+
+        Args:
+            seq_obj
+        Returns:
+            tensor: hidden states at event times.
+        """
+
+        # first, prepare the prev_dtime_seqs, which is shifted to the right by 1 and padding on the first position
+        # we pad the target part of the sequence as well
+        dtime_seqs = seq_obj.dtime_seqs[:, -self.src_seq_len-self.tgt_seq_len:]
+        prev_dtime_seqs = torch.zeros_like(dtime_seqs)
+        prev_dtime_seqs[:, 1:] = dtime_seqs[:, :-1]
+        prev_dtime_seqs[:, 0] = self.PAD_TOKEN
+        prev_dtime_seqs[:, self.src_seq_len:] = self.PAD_TOKEN
+
+        # fix interarrival_time_seqs and time_seqs
+        interarrival_time_seqs = seq_obj.interarrival_time_seqs[:, -self.src_seq_len-self.tgt_seq_len:]
+        time_seqs = seq_obj.time_seqs[:, -self.src_seq_len-self.tgt_seq_len:] # it is never used
+        len_seqs = seq_obj.len_seqs[:, -self.src_seq_len-self.tgt_seq_len:]
+
+        # now that we have all sequences ready, we should replace the tgt part of some of the sequences with the paddings
+        slot_seqs = torch.cat(
+            [
+                seq_obj.src_slot_seqs, 
+                seq_obj.tgt_slot_seqs if self.include_slot_in_tgt else self.slots_pad_id * torch.ones_like(seq_obj.tgt_dtime_seqs, device=self.device, dtype=torch.long)
+            ],
+            dim=1
+        )
+
+        mcs_seqs = torch.cat(
+            [
+                seq_obj.src_mcs_seqs, 
+                seq_obj.tgt_mcs_seqs if self.include_mcs_in_tgt else self.mcs_pad_id * torch.ones_like(seq_obj.tgt_dtime_seqs, device=self.device, dtype=torch.long)
+            ],
+            dim=1
+        )
+        mretx_seqs = torch.cat(
+            [
+                seq_obj.src_mretx_seqs, 
+                seq_obj.tgt_mretx_seqs if self.include_mretx_in_tgt else self.mretx_pad_id * torch.ones_like(seq_obj.tgt_dtime_seqs, device=self.device, dtype=torch.long)
+            ],
+            dim=1
+        )
+        rfailed_seqs = torch.cat(
+            [
+                seq_obj.src_rfailed_seqs, 
+                seq_obj.tgt_rfailed_seqs if self.include_rfailed_in_tgt else self.rfailed_pad_id * torch.ones_like(seq_obj.tgt_dtime_seqs, device=self.device, dtype=torch.long)
+            ],
+            dim=1
+        )
+
+        # apply embedding on the whole sequences (seq_len = src + tgt)
+        embeddings = self.delay_embedding(
+            prev_dtime_seqs,  # prev_dtime
+            time_seqs, 
+            interarrival_time_seqs, 
+            slot_seqs, 
+            mcs_seqs, 
+            mretx_seqs, 
+            rfailed_seqs, 
+            len_seqs
+        )
+        # embedding dims: [batch_size, seq_len, d_model]
+
+        # return the embeddings
+        return embeddings
+
+    def forward(self, seq_obj : SequenceSeperate, phase=None):
+
+        # apply embedding on the delay sequence
+        embeddings = self.embed(seq_obj)
+        # embeddings dim: [batch_size, seq_len = src_len + tgt_len, d_model]
+
+        # autoregressive prediction
+        # extract the src embedding
+        embeddings_src = embeddings[:, -self.src_seq_len-self.tgt_seq_len:-self.tgt_seq_len, :]
+
+        # feed in the src data
+        rnn_out, prev_hidden = self.layer_rnn(embeddings_src)
+        # [batch_size, his_len, d_model]
+
+        # [batch_size, 1, d_model]
+        all_preds = torch.stack([ rnn_out[:, -1, :] ], dim=1)
+        mdn_params = self.mdn_head(all_preds)
+
+        batch_size, tgt_seq_len = seq_obj.tgt_non_pad_mask.float().size() # we predict only 1 step here
+        num_predictions = batch_size
+        return mdn_params, num_predictions
+
+
+    def loglike_loss(self, batch, phase):
+
+        seq_obj = SequenceSeperate(batch, self.device, self.src_seq_len, self.tgt_seq_len, self.delay_embedding.dtime_transform, self.delay_embedding.len_transform, self.delay_embedding.interarrival_time_transform)
+
+        mdn_params, num_predictions = self.forward(seq_obj, phase)
+        # mdn_params: [batch_size, 1, self.mdn.num_params]
+
+        if phase == RunnerPhase.TRAIN or phase == RunnerPhase.VALIDATE:
+            labels = seq_obj.tgt_dtime_seqs_transformed[:, :1]
+            # labels: [batch_size, 1]
+
+            nll, nll_mask = self.mdn.negative_loglikelihood(mdn_params, labels, seq_obj.tgt_non_pad_mask[:, :1])
+            num_predictions_nll = nll_mask.sum(axis=0)
+            assert np.array_equal(num_predictions, sum(num_predictions_nll.cpu().numpy()))
+
+        else:  
+            # repeat the mdn_params for the tgt sequence length
+            mdn_params = mdn_params.repeat(1, self.tgt_seq_len, 1)
+            # output: [batch_size, tgt_seq_len, self.mdn.num_params]
+
+            labels = seq_obj.tgt_dtime_seqs_transformed
+            # labels: [batch_size, tgt_seq_len]
+
+            nll, nll_mask = self.mdn.negative_loglikelihood(mdn_params, labels, seq_obj.tgt_non_pad_mask)
+            num_predictions_nll = nll_mask.sum(axis=0)
+            assert np.array_equal(num_predictions*self.tgt_seq_len, sum(num_predictions_nll.cpu().numpy()))
+
+        return nll, nll_mask
+
+
+    def predict(self, batch):
+
+        seq_obj = SequenceSeperate(batch, self.device, self.src_seq_len, self.tgt_seq_len, self.delay_embedding.dtime_transform, self.delay_embedding.len_transform, self.delay_embedding.interarrival_time_transform)
+        label = seq_obj.tgt_dtime_seqs_transformed
+
+        interarrival_time_src_seqs = seq_obj.src_interarrival_time_seqs_transformed
+        len_src_seqs = seq_obj.src_len_seqs_transformed
+
+        mdn_params, num_predictions = self.forward(seq_obj)
+        # mdn_params: [batch_size, 1, self.mdn.num_params]
+        # repeat the mdn_params for the tgt sequence length
+        mdn_params = mdn_params.repeat(1, self.tgt_seq_len, 1)
+        # output: [batch_size, tgt_seq_len, self.mdn.num_params]
+
+
+        pred_mean, pred_var = self.mdn.mean_variance(mdn_params)
+
+        pred_q99a = self.mdn.quantile(mdn_params,q=0.005)
+        pred_q99b = self.mdn.quantile(mdn_params,q=0.995)
+
+        pred_q9a = self.mdn.quantile(mdn_params,q=0.05)
+        pred_q9b = self.mdn.quantile(mdn_params,q=0.95)
+
+        pred_q7a = self.mdn.quantile(mdn_params,q=0.15)
+        pred_q7b = self.mdn.quantile(mdn_params,q=0.85)
+
+        pred_q5a = self.mdn.quantile(mdn_params,q=0.25)
+        pred_q5b = self.mdn.quantile(mdn_params,q=0.75)
+
+        pred_mask = seq_obj.tgt_non_pad_mask
+
+        assert label.shape == pred_mean.shape
+        assert label.shape == pred_var.shape
+
+        return (pred_mean, pred_var, pred_q5a, pred_q5b, pred_q7a, pred_q7b, pred_q9a, pred_q9b, pred_q99a, pred_q99b), label, (interarrival_time_src_seqs, len_src_seqs), pred_mask
